cc89f1b7e3477c2d5d472a9cf811297adb340343
Gary Mendonca <mendonca_gary@ymail.com>
2017-06-07 13:45:54 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/mypc/feedreader/RssFeedModel.java
@@ -0,0 +1,18 @@
+package com.example.mypc.feedreader;
+
+/**
+ * Created by MYPC on 06-06-2017.
+ */
+
+public class RssFeedModel {
+
+    public String title;
+    public String link;
+    public String description;
+
+    public RssFeedModel(String title, String link, String description) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+    }
+}
